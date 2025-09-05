08bf4f9717300b332c269c9a7c904751bdf84adb
Lucas Boarim Paludo <lucaspaludo2018@gmail.com>
2025-09-05 07:29:34 -0300

    Add files via upload

--- Alunos/Luiz Ricardo/Asteroides/jogo_final/jogoFinal.py
+++ Alunos/Luiz Ricardo/Asteroides/jogo_final/jogoFinal.py
@@ -26,11 +26,23 @@ ALTURA_NAVE = imagenave_espacial.get_height()
 LARGURA_RAIO = imagemraio.get_width()
 ALTURA_RAIO = imagemraio.get_height()
 
-def moverJogador():
-    pass
+def moverJogador(jogador, teclas, dim_janela):
+    borda_esquerda = 0
+    borda_superior = 0
+    borda_direita = dim_janela[0]
+    borda_inferior = dim_janela[1]
+    if teclas['esquerda'] and jogador['objRect'].left > borda_esquerda:
+        jogador['objRect'].x -= jogador['vel']
+    if teclas['direita'] and jogador['objRect'].right < borda_direita:
+        jogador['objRect'].x += jogador['vel']
+    if teclas['cima'] and jogador['objRect'].top > borda_superior:
+        jogador['objRect'].y -= jogador['vel']
+    if teclas['baixo'] and jogador['objRect'].bottom < borda_inferior:
+        jogador['objRect'].y += jogador['vel']
 
-def moverElemento():
-    pass
+def moverElemento(elemento):
+    elemento['objRect'].x += elemento['vel'][0]
+    elemento['objRect'].y += elemento['vel'][0]
 
 def terminar():
     pygame.quit()
@@ -79,7 +91,7 @@ aguardaEntrada()
 recorde = 0
 while True:
     asteroides = []
-    raio = []
+    raios = []
     pontuacao = 0
     deve_contunuar = False 
     teclas = {
@@ -102,8 +114,54 @@ while True:
         pontuacao += 1
         if pontuacao == recorde:
             somRecorde.play()
-
-
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                terminar()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_ESCAPE:
+                    deve_contunuar = False
+                if  evento.key == pygame.K_LEFT or evento.key == pygame.K_a:
+                    teclas['esquerda'] = True
+                if evento.key == pygame.K_RIGHT or evento.key == pygame.K_d:
+                    teclas['direita'] = True
+                if evento.key == pygame.K_UP or evento.key == pygame.K_w:
+                    teclas['cima'] = True
+                if evento.key == pygame.K_DOWN or evento.key == pygame.K_s:
+                    teclas['baixo'] = True
+                if evento.key == pygame.K_SPACE:
+                    raio = {
+                        'objRect': pygame.Rect(jogador['objRect'].centerx, jogador['objRect'].top, LARGURA_RAIO, ALTURA_RAIO),
+                        'vel':VEL_RAIO,
+                        'imagem': imagemraio
+                }
+                    raios.append(raio)
+                    somTiro.play()
+            if evento.type == pygame.KEYUP:
+                if  evento.key == pygame.K_LEFT or evento.key == pygame.K_a:
+                    teclas['esquerda'] = False
+                if evento.key == pygame.K_RIGHT or evento.key == pygame.K_d:
+                    teclas['direita'] = False
+                if evento.key == pygame.K_UP or evento.key == pygame.K_w:
+                    teclas['cima'] = False
+                if evento.key == pygame.K_DOWN or evento.key == pygame.K_s:
+                    teclas['baixo'] = False
+                
+            if evento.type == pygame.MOUSEMOTION:
+                centerX_jogador = jogador['objRect'].centerx  
+                centerY_jogador = jogador['objRect'].centery
+                jogador['objRect'].move_ip(evento.pos[0] - centerX_jogador, evento.pos[1] - centerY_jogador)
+        
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                raio =  {
+                        'objRect': pygame.Rect(jogador['objRect'].centerx, jogador['objRect'].top, LARGURA_RAIO, ALTURA_RAIO),
+                        'vel':VEL_RAIO,
+                        'imagem': imagemraio
+                }
+                raios.append(raio)
+                somTiro.play()
+        
+        
+        
         janela.blit(imagemFundoRedim, (0, 0))
         colocarTexto(f"Pontuação: {str(pontuacao)}", fonte, janela, 10, 0)
         colocarTexto(f"Recorde:{str(recorde)}", fonte, janela, 10, 40)
